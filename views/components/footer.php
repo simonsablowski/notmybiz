6480043ffa895a7d7272b37b1a4cad017e429899
simsab@simsab.de <simsab@simsab.de@e7e5af5d-9d1d-875f-1599-4cce213e0994>
2011-07-09 23:35:00 +0000

    - fixed analytics integration - removed footer link to motivado.com - added js functionality for opening external links in a new tab - added logo image - minor changes

--- views/components/footer.php
+++ views/components/footer.php
@@ -7,13 +7,10 @@
 				</p>
 				<ul id="menu">
 					<li class="menu-item">
-						<a class="external" href="http://motivado.com" title="motivado.com">motivado.com</a>
+						<a href="http://fbrccn.com" title="fbrccn.com">fbrccn.com</a>
 					</li>
 					<li class="menu-item">
-						<a class="external" href="http://fbrccn.com" title="fbrccn.com">fbrccn.com</a>
-					</li>
-					<li class="menu-item">
-						<a class="external" href="http://simsab.de/en" title="simsab.de">simsab.de</a>
+						<a href="http://simsab.de/en" title="simsab.de">simsab.de</a>
 					</li>
 				</ul>
 			</div>
@@ -21,7 +18,10 @@
 		<script type="text/javascript">
 		<!--
 		var gaJsHost = 'https:' == document.location.protocol ? 'https://ssl.' : 'http://www.';
-		document.write(unescape('%3Cscript src="' + gaJsHost + 'google-analytics.com/ga.js" type="text/javascript"%3E%3C/script%3E'));
+		document.write(unescape('%3Cscript src="' + gaJsHost + 'google-analytics.com/ga.js" type="text/javascript"%3E%3C/script%3E'));		//-->
+		</script>
+		<script type="text/javascript">
+		<!--
 		try {
 			var pageTracker = _gat._getTracker('UA-2644687-1');
 			pageTracker._initData();
