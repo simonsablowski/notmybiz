1c8a6ef4f5d192678be69119ea79cccb9ed3dbd3
Simon Sablowski <contact@simsab.net>
2013-11-15 11:31:50 +0000

    - major redesign - passing page-specific javascript files to header - correcting white spaces in gallery album breadcrumbs - getting rid of two-column image preview layout - combining stylesheets - correcting css standards - making tracking code configurable - checking in Request and Image configuration

--- views/web/components/header.php
+++ views/web/components/header.php
@@ -8,10 +8,13 @@
 		<meta name="description" content="<? echo (isset($subtitle) ? $this->localize($subtitle) . ' - ' : '') . $this->localize('notmybiz.com presents illustration and art by Simon Sablowski and Filippo Baraccani'); ?>"/>
 		<meta name="keywords" content="<? echo $this->localize('simon sablowski, sablowski, berlin, filippo baraccani, baraccani, bremen, portfolio, illustration, design, life drawing, character design, portrait, caricature, comic'); ?>"/>
 		<meta name="revisit-after" content="1 day"/>
-		<link href="<? echo $this->getApplication()->getConfiguration('basePath'); ?>css/style.css" rel="stylesheet" title="Default" type="text/css"/>
+		<link href="<? echo $this->getApplication()->getConfiguration('basePath'); ?>css/notmybiz.css" rel="stylesheet" title="Default" type="text/css"/>
 		<link type="image/x-icon" href="<? echo $this->getApplication()->getConfiguration('basePath'); ?>favicon.ico" rel="shortcut icon"/>
 		<script type="text/javascript" src="<? echo $this->getApplication()->getConfiguration('basePath'); ?>js/jquery-1.3.2.js"></script>
 		<script type="text/javascript" src="<? echo $this->getApplication()->getConfiguration('basePath'); ?>js/notmybiz.js"></script>
+<? if (isset($javaScriptFiles)) foreach ($javaScriptFiles as $javaScriptFile): ?>
+		<script type="text/javascript" src="<? echo $javaScriptFile; ?>"></script>
+<? endforeach; ?>
 	</head>
 	<body>
 		<div id="document">
