9e434405ef56a98fd5f466881e41961866b24042
Simon Sablowski <contact@simsab.net>
2014-02-18 23:09:53 +0000

    adding disclaimer

--- views/web/components/footer.php
+++ views/web/components/footer.php
@@ -1,8 +1,8 @@
 		</div>
 		<footer id="footer">
 			<p id="copyright">
-				<?php echo $this->localize('&copy; 2004-%d <a href="%s">notmybiz</a>', array(date('Y'), $this->getConfiguration('basePath'))); ?>
-
+				<?php echo $this->localize('copyright', array(date('Y'))); ?> <a href="<?php echo $this->getConfiguration('basePath'); ?>"><?php echo $this->localize('document-title'); ?></a>
+				&mdash; <a href="<?php echo $this->getConfiguration('basePath'); ?>disclaimer"><?php echo $this->localize('disclaimer-title'); ?></a>
 			</p>
 		</footer>
 <?php echo $this->getConfiguration('trackingCode'); ?>
---
+++ views/web/disclaimer.php
@@ -0,0 +1,77 @@
+<?php $this->displayView('components/header.php'); ?>
+			<article id="body">
+				<h2>
+					<?php echo $this->localize('disclaimer-title'); ?>
+
+				</h2>
+				<div>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block1-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block1-paragraph1'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block2-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block2-paragraph1'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block3-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block3-paragraph1'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block4-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block4-paragraph1'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block5-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block5-paragraph1'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block6-title'); ?></strong>
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block6-paragraph1'); ?>
+
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block6-paragraph2'); ?>
+
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block6-paragraph3'); ?>
+
+					</p>
+					<p>
+						<?php echo $this->localize('disclaimer-block6-paragraph4'); ?>
+
+					</p>
+					<p>
+						<strong><?php echo $this->localize('disclaimer-block7-title'); ?></strong>
+					</p>
+					<ul>
+						<li>
+							<?php echo $this->localize('disclaimer-block7-paragraph1'); ?>
+
+						</li>
+						<li>
+							<?php echo $this->localize('disclaimer-block7-paragraph2'); ?>
+
+						</li>
+					</ul>
+				</div>
+			</article>
+<?php $this->displayView('components/footer.php'); ?>
